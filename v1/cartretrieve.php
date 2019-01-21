e937f792808b6d9114bad049a205c5134e1ca2b6
Justice <popystone@gmail.com>
2019-01-21 11:40:25 +0000

    Added message and error checking to the cart retrieve php

--- v1/cartretrieve.php
+++ v1/cartretrieve.php
@@ -10,21 +10,28 @@
     $db = new DbOperations();
     //$customer = $_POST('customer');
     
-    $products = $db->getCartItems($_POST['customer']);
+    if($db->getCartItems($_POST['customer'])){
+        $products = $db->getCartItems($_POST['customer']);
 
-        
-    foreach($products as $product){
-        $response['id'] = $product['id'];
-        $response['name'] = $product['name'];
-        $response['description'] = $product['description'];
-        $response['quantity'] = $product['quantity'];
-        $response['price'] = $product['price'];
-        $response['imageUrl'] = $product['img_url'];
-        $response['customerId'] = $product['customer'];
-        $response['productId'] = $product['product_id'];
+            
+        foreach($products as $product){
+            $response['error'] = false; 
+			$response['message'] = "Retrieval successful";
+            $response['id'] = $product['id'];
+            $response['name'] = $product['name'];
+            $response['description'] = $product['description'];
+            $response['quantity'] = $product['quantity'];
+            $response['price'] = $product['price'];
+            $response['img_url'] = $product['img_url'];
+            $response['customer'] = $product['customer'];
+            $response['product_id'] = $product['product_id'];
 
-        $results[] = $response;
-        $output['cart_items'] = $results;
+            $results[] = $response;
+            $output['cart_items'] = $results;
+        }
+    }else{
+        $response['error'] = true; 
+		$response['message'] = "Required fields are missing";
     }
     
     echo json_encode($output);
